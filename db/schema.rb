c76c98d0f212f07487e756d1bc25f7721b660aa3
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-19 22:00:08 +1000

    Added Orders model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019081933) do
+ActiveRecord::Schema.define(version: 20171019115849) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "product_id"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20171019081933) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "cart_products_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_products_id"], name: "index_orders_on_cart_products_id"
   end
 
   create_table "products", force: :cascade do |t|
