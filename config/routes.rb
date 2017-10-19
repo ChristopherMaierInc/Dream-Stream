9af7438364486814bac81aba6a0f989206f2aee1
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-20 05:36:15 +1000

    Fixed route names

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,13 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index]
   resource :cart, only: [:show]
-  resources :order_items, only: [:create, :update, :destroy]
+  resources :cart_products, only: [:create, :update, :destroy]
 
   root to: "products#index"
 
-  get 'checkout', to: 'checkout#new', as: 'new_order'
+  get 'checkout', to: 'checkout#new', as: 'new_purchase'
 
-  get 'checkout', to 'checkout#purchase', as "purchase_order"
-
-  # get 'pages/home', to: 'pages#home', as: 'home'
-  # 
-  # get 'pages/checkout', to: 'pages#checkout', as: 'checkout'
-  #
-  # post 'pages/checkout', to: 'pages#purchase', as: 'purchase'
+  get 'checkout', to 'checkout#purchase', as "purchase_finalized"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
