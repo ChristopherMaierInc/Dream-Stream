babb636ee81d577c043bfa142a69065311436c4d
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-20 13:49:00 +1000

    Setup postgresql as main db. Fixed up mailers and cart page.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -13,8 +13,7 @@ class CheckoutController < ApplicationController
     charge = Stripe::Charge.create(
       :customer    => customer.id,
       :amount      => @price_in_cents,
-
-      :description => current_cart.cart_products
+      :description => current_cart.cart_products,
       :currency    => 'AUD'
     )
 
@@ -22,11 +21,11 @@ class CheckoutController < ApplicationController
 
     notification = "Customer [#{charge.customer}] with email #{customer.email} just purchased #{charge.description} for $#{@price_in_dollars} #{charge.currency}"
 
-    PurchaseMailer.mail_purchase_notification(customer.email, notnotification).deliver_now
+    PurchaseMailer.mail_purchase_notification(customer.email, notification).deliver_now
 
-    confirmation = "Your order #{@order} was successfully made and your card was charged for $#{@price_in_dollars}. The order status is currently: #{current_cart.order_statuses.id}. Please contact us on our website if you experience any issues."
+    confirmation = "Your order #{@order} was successfully made and your card was charged for $#{@price_in_dollars}. Please contact us on our website if you experience any issues."
 
-    PurchaseMailer.mail_purchase_confirmation(customer_email, confirmation).deliver_now
+    PurchaseMailer.mail_purchase_confirmation(customer.email, confirmation).deliver_now
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,14 +1,14 @@
 class PurchaseMailer < ApplicationMailer
   def mail_purchase_notification(customer_email, message)
     @customer_email = customer_email
-    @message = notification
+    @message = message
     shop_email = 'filip.maslovaric@gmail.com'
     mail(to: shop_email, subject: "Customer @ #{@customer_email} just made a purchase!")
   end
 
   def mail_purchase_confirmation(customer_email, message)
     @customer_email = customer_email
-    @message = confirmation
+    @message = message
     mail(to: @customer_email, subject: "Your order was successful!")
   end
 end
