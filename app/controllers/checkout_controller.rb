4c3b684b15b6b22aa64f5b8a77ec795bb2f6cb5e
FilipMaslovaric <filip.maslovaric@gmail.com>
2017-10-20 11:52:19 +1000

    added cart to stripe payments

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,7 +1,25 @@
 class CheckoutController < ApplicationController
-  def new
-  end
+  def create
+    # Amount in cents
+    @price_in_dollars = current_cart.subtotal
+    @price_in_cents = (@price_in_dollars * 100).to_i
 
-  def purchase
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @price_in_cents,
+      :description => 'Rails Stripe customer',
+      :currency    => 'AUD'
+    )
+
+    flash[:notice] = "Your purchase was successful. Your card was charged $#{@price_in_dollars}"
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to '/carts/show'
   end
 end
